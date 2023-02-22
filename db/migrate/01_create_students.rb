98f90c76f30f65916adad6e79b1d06a2d0a85eae
tugi <blueqwinn@gmail.com>
2023-02-22 08:53:41 +0300

    add created data

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,39 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    creat_table : Students do |t|
+      t.string :name
+      t.text :age
+
+      t.height
+    
+    
+      def change
+        reversible do |dir|
+        change_table :students do |t|
+        dir.up { t.change : age, :string}
+        dir.down{t.change :age, :integer}
+    
+      end
+    end
+  end
+
+
+        def  up
+          change_table :students do |t|
+  t.change :age, string
+          end
+        end
+
+        def down
+          change_table :students do t||
+     t.change :age, integer
+
+                end
+             end
+          end
+    end
   end
 end
+
+
+
