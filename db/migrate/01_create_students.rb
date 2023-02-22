bc6f624e062f6086bce3708092092f0ef9961c07
tugi <blueqwinn@gmail.com>
2023-02-22 15:26:41 +0300

    add changed files

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -6,31 +6,54 @@ class CreateStudents < ActiveRecord::Migration[6.1]
 
       t.height
     
-    
-      def change
-        reversible do |dir|
-        change_table :students do |t|
-        dir.up { t.change : age, :string}
-        dir.down{t.change :age, :integer}
-    
-      end
+   class AddPartNumberToStudents< ActiveRecord::Migration[6.0]
+ def change
+  add_column :students, :number_students, string
+  add_index :products, :number_students
+ end
+
+end
+  class   AddDetailsToStudents<ActiveRecord::Migration[6.0]
+ def change 
+ add_column :students :number_students, :string
+ add_column :students, :number_students, :group
+    end
+end
+
+class ChangeStudent < AciveRecord[6.0]
+  def change
+    reversible do |dir|
+    change_table :students do |t|
+    dir.up { t.change : age, :string}
+    dir.down{t.change :age, :integer}
+
+  end
+end
+end
+
+
+class ChangeStudentTableData < ActiveRecord::Migration[6.0]
+  def  up
+    change_table :students do |t|
+t.change :age, string
     end
   end
 
+  def down
+    change_table :students do t||
+t.change :age, integer
+end
+end
+end
 
-        def  up
-          change_table :students do |t|
-  t.change :age, string
-          end
+class AddPartNumberToProducts < ActiveRecord::Migration[6.0]
+  def change 
+        remove_column: students, number_students, :string
+      
         end
+    end
 
-        def down
-          change_table :students do t||
-     t.change :age, integer
 
-                end
-             end
-          end
     end
   end
 end
